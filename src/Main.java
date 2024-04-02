c03ac6f59938da9a92e577e2b01395396f329c4b
maheraa <maheraasin7@gmil.com>
2024-04-01 21:51:19 -0400

    my change

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.print("Hello World, sayfore, hi");
-        System.out.println("hello");
+        System.out.println("hello ths is maheara");
     }
 }
